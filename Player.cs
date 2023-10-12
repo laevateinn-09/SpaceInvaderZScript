f21b1f4e5877b7f3081a1e3b84a55c528b775689
Albert <91774625+laevateinn-09@users.noreply.github.com>
2023-10-12 15:02:03 +0700

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 5f;
+    public Projectile laserPrefab;
+    public System.Action killed;
+    public bool laserActive { get; private set; }
+
+    private void Update()
+    {
+        Vector3 position = transform.position;
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            position.x -= speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            position.x += speed * Time.deltaTime;
+        }
+
+        Vector3 leftEdge = Camera.main.ViewportToWorldPoint(Vector3.zero);
+        Vector3 rightEdge = Camera.main.ViewportToWorldPoint(Vector3.right);
+        position.x = Mathf.Clamp(position.x, leftEdge.x, rightEdge.x);
+        transform.position = position;
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        if (!laserActive){
+            laserActive = true;
+
+            Projectile laser = Instantiate(laserPrefab, transform.position, Quaternion.identity);
+            laser.destroyed += OnLaserDestroyed;
+        }
+    }
+
+    private void OnLaserDestroyed(Projectile laser)
+    {
+        laserActive = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Missile") ||
+            other.gameObject.layer == LayerMask.NameToLayer("Invader"))
+        {
+            if (killed != null) {
+                killed.Invoke();
+            }
+        }
+    }
+
+}
